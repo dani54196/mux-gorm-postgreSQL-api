0d809893a28bc0590c9133e20745a3e5bd6da4b1
dani54196 <dani54196@gmail.com>
2023-02-20 12:48:31 -0300

    db connection

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+
+	"github.com/dani54196/mux-gorm-postgreSQL-api/db"
+	"github.com/dani54196/mux-gorm-postgreSQL-api/routes"
 )
 
 func main() {
-	fmt.Println("holis")
+	// enviroment var
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// database connection
+	db.DBConnection()
+
+	r := mux.NewRouter()
+
+	// routes
+	r.HandleFunc("/", routes.HomeHandler)
+
+	port := os.Getenv("PORT")
+	http.ListenAndServe(":"+port, r)
 }
